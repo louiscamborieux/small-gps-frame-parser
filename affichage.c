d8f842b202a735a9170278e2917780adaca7e1d2
Camborieux Louis <louis.camborieux@etu.iut-tlse3.fr>
2022-05-01 19:47:47 +0200

    Mise a jour affichage, lecture d'un fichier texte pour les trames en entrée

--- affichage.c
+++ affichage.c
@@ -1,28 +1,3 @@
-/******************************************************************************
-*  ASR => 4R2.04                                                              *
-*******************************************************************************
-*                                                                             *
-*  N° de Sujet : 1                                                            *
-*                                                                             *
-*******************************************************************************
-*                                                                             *
-*  Intitulé :   Analyse de trames GPS                                         *
-*                                                                             *
-*******************************************************************************
-*                                                                             *
-*  Nom-prénom1 : Afrite Yanis                                                 *
-*                                                                             *
-*  Nom-prénom2 : Calmet Alexandre                                             *
-*                                                                             *
-*  Nom-prénom3 : Bessier Jason                                                *
-*                                                                             *
-*  Nom-prénom4 : Camborieux Louis                                             *
-*                                                                             *
-*******************************************************************************
-*                                                                             *
-*  Nom du fichier : affichage.c                                               *
-*                                                                             *
-******************************************************************************/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -32,34 +7,99 @@ int affichercoord (char* s, char coord );
 int afficherheure(char* s);
 
 int main () {
-    char* stest = "$GPGGA,064036.289,4836.5355,N,00740.9373,E,1,04,3.2,200.2,M,,,,0000*0E";
     
-    printf("-----------------------------------Affichage-----------------------------------\n");
-    printf("\nTrame : %s\n",stest);
-    printf("\nVerification...\n");
-    switch (checkframe (stest)) {
-            case -1:  printf("Trame au format inccorect\n");
+    //Ouverture du fichier in.txt à partir du quel sont entrées les trames
+    FILE *fic;
+    fic = fopen("in.txt", "r");
+    if (fic == NULL)
+    {
+        printf("Fichier in.txt inexistant, veuillez renseigner vos trames dans un fichier nommé \"in.txt\"");
+        return -3;
+    }
+    fclose(fic);
+        
+    char * ligne = NULL;
+    size_t taille = 0;
+    char entree ;
+    int page = 0;
+
+    //Affichage des lignes du fichier avec un système de pagination
+    while ( atoi(&entree) ==0 || atoi(&entree) >5) {
+    fic = fopen("in.txt", "r");
+    int pi =0;
+    while (pi < page*5 && !feof(fic)) {
+        getline(&ligne,&taille,fic);
+        pi++;
+    }
+    getline(&ligne,&taille,fic); 
+    printf("---------------------------------Page %d----------------------------------\n",page+1) ;
+    int i = 0;  
+    //Affichage page en cours
+    while (!feof(fic) && i <(4)) {
+        printf("%d) ",i+1);
+        printf(ligne);
+        getline(&ligne,&taille,fic);  
+        i++;
+    }
+    printf("%d) %s",i+1,ligne);
+    if (i ==0) {
+        printf("Fichier vide");
+        return -4;
+    }
+    //Menu sous la forme de saisie au clavier
+    printf("\nChoisir une trame à analyser | 1-%d : Choix de la trame, s : Page suivante, p : Page précedente\n",i+1);
+    entree = getchar();
+    while ((getchar()) != '\n');
+    
+    switch (entree) {
+        case 'p':
+            if (page !=0) {
+                page-=1;
+            }
+                break;
+        case 's':
+            if  (!feof(fic)) {
+                page++;
+            }
+                break;
+        default :
+            break;
+    }
+    fclose(fic);
+    
+    }
+    //Récuperation de la ligne sélectionnée
+    fic = fopen("in.txt", "r");
+    int i = 0;
+    while (i < page*5+atoi(&entree) && !feof(fic)) {
+        getline(&ligne,&taille,fic);
+        i++;
+    }
+    
+    printf("------------------------------------Analyse-----------------------------------\n");
+    printf("\nTrame : %s\n",ligne);
+    printf("\nVérification...\n");
+    switch (checkframe (ligne)) {
+            case -1:  printf("Trame au format incorrect.\n");
                 return -1;
                 break; 
             case 0: printf("Trame OK\n");
                 break;
-            default: printf("Il manque %d champs dans cette trame\n",15-checkframe(stest));
-                return checkframe(stest);
+            default: printf("Il manque %d champ(s) dans cette trame.\n",14-checkframe(ligne));
+                return checkframe(ligne);
                 break;
     }
     
-    printf("\n    Informations extraites    \n");
-    printf("\n-----------------------------\n| ");
-    affichercoord(stest, '0');
+    printf("\n Informations extraites\n");
+    printf("\n------------------------------\n| ");
+    affichercoord(ligne, '0');
     printf(" |\n| ");
-    affichercoord(stest, '1');
-    printf(" |\n|---------------------------|\n| ");
-    afficherheure(stest);
-    printf("         |\n-----------------------------\n");
+    affichercoord(ligne, '1');
+    printf(" |\n------------------------------\n| ");
+    afficherheure(ligne);
+    printf("    |\n------------------------------\n");
     return 0;
-
-
-    
+   
 }
 
 int affichercoord (char* s, char coord ) {
@@ -81,10 +121,10 @@ int affichercoord (char* s, char coord ) {
         extract_fields(s, champs);
         switch (coord) {
             case '0' : convert_coord(champs[1],chaine,0);
-                        printf("Lattitude : %s",chaine);
+                        printf("Lattitude : %*s",15,chaine);
                         break;
             case '1' : convert_coord(champs[2],chaine,1);
-                        printf("Longitude : %s",chaine);
+                        printf("Longitude : %*s",15,chaine);
                         break;
             case 'a' : convert_coord(champs[1],chaine,0);
                         printf("Lattitude : %s",chaine);
@@ -120,5 +160,5 @@ int afficherheure (char* s) {
         extract_fields(s, champs);
 
         format_time(champs[0],chaine);
-        printf("Heure : %s",chaine);
+        printf("Heure     : %*s",11,chaine);
         }
